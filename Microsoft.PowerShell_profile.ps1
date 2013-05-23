303335f390c022a1b485117145f2742a6b83b6d1
Will Rideout <william.rideout@gmail.com>
2013-05-23 16:41:06 -0400

    adjusted the edit command

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -32,33 +32,34 @@ function Edit-File
 {
 	$file = $args[0]
 	if(Test-Path "C:\Users\$user\Dropbox\Tools\Notepad++\notepad++.exe")
-    {
-        & "C:\Users\$user\Dropbox\Tools\Notepad++\notepad++.exe" $file
-    }
+	{
+        	& "C:\Program Files (x86)\Notepad++\notepad++.exe" $file
+    	}
 	else
 	{
-		throw "Notepad++ not installed or bad path to executable"
+		# default to using notepad if notepad++ is not installed
+		notepad $file
 	}
 }
 
 function Touch-File
 {
-    $file = $args[0]
-    if($file -eq $null)
-    {
-        throw "No filename supplied"
-    }
+	$file = $args[0]
+	if($file -eq $null)
+	{
+		throw "No filename supplied"
+	}
     
 	# Simply update the date on the file if the file exists, otherwise make a
 	# new empty file with the specified name
-    if(Test-Path $file)
-    {
-        (Get-ChildItem $file).LastWriteTime = Get-Date
-    }
-    else
-    {
-        New-Item $file -ItemType File
-    }
+	if(Test-Path $file)
+	{
+		(Get-ChildItem $file).LastWriteTime = Get-Date
+	}
+	else
+	{
+		New-Item $file -ItemType File
+	}
 }
 
 function Get-WebObject
@@ -76,6 +77,45 @@ function Get-WebObject
 	}
 }
 
+function Change-Directory
+{
+	# Check for too many args
+	if($args.Count -gt 1)
+	{
+		throw "Too many arguments supplied"
+	}
+
+	# Record where we are
+	$tmp = pwd
+
+	# Mimic the bash behavior and simply go home if no args supplied
+	if($args.Count -eq 0)
+	{
+		Set-Location ~
+	}
+	else
+	{	
+		# Check for '-', which means go to previous directory
+		if($args[0] -eq '-')
+		{
+			$pwd = $PREV_DIR
+		}
+		else
+		{
+			$pwd = $args[0]
+		}
+
+		# Go to the appropriate directory
+		if(Test-Path $pwd)
+		{
+			Set-Location $pwd
+		}
+	}
+	
+	# Save the previous directory
+	Set-Variable -Name PREV_DIR -Value $tmp -Scope global
+}
+
 ################################################################################
 # Aliases
 ################################################################################
@@ -83,4 +123,7 @@ Set-Alias touch Touch-File
 Set-Alias tools Get-Tools
 Set-Alias edit Edit-File
 Set-Alias wget Get-WebObject
-Set-Alias la Get-Content -force
+
+# Replace the cd alias with our own
+Remove-Item Alias:cd
+Set-Alias cd Change-Directory
